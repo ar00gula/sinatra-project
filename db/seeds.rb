d3cdbf79ba90c0eb7fc443877b8fb90c4bbfd1ee
ar00gula <arianna.mayer@gmail.com>
2020-11-18 18:22:18 -0500

    user edit tags still broken, seeds updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,24 +13,96 @@ books_list = {
       :img_url => "../images/slippery-creatures.png"
     },
     "The Sugared Game" => {
-        :author => "KJ Charles"
+        :author => "KJ Charles",
+        :summary => "It’s been two months since bookseller Will Darling saw Kim Secretan and he doesn’t expect to see him again. What do a rough and ready ex-soldier and a disgraced shady aristocrat have to do with each other anyway?<br><br>
+
+          But when Will encounters a face from the past in a disreputable nightclub, Kim turns up again—as shifty, unreliable, and irresistible as ever. And before Will knows it, he’s been dragged back into Kim’s shadowy world of criminal conspiracies and underhand dealings.<br><br>
+          
+          This time, though, things are underhand even by Kim standards. This time, the danger is too close to home. And if Will and Kim can’t find common ground against unseen enemies, they risk losing everything.<br><br>
+          
+          <i>A 1920s m/m romance trilogy in the spirit of Golden Age pulp fiction.</i>",
+        :img_url => "../images/sugared-game.jpg"
     },
     "The Magpie Lord" => {
-        :author => "KJ Charles"
-    },"A Case Of Posession" => {
-        :author => "KJ Charles"
+        :author => "KJ Charles",
+        :summary => "<b>A lord in danger. A magician in turmoil. A snowball in hell.</b><br><br>
+
+        <i>A Charm of Magpies, Book 1</i><br><br>
+        
+        Exiled to China for twenty years, Lucien Vaudrey never planned to return to England. But with the mysterious deaths of his father and brother, it seems the new Lord Crane has inherited an earldom. He’s also inherited his family’s enemies. He needs magical assistance, fast. He doesn’t expect it to turn up angry.<br><br>
+        
+        Magician Stephen Day has good reason to hate Crane’s family. Unfortunately, it’s his job to deal with supernatural threats. Besides, the earl is unlike any aristocrat he’s ever met, with the tattoos, the attitude…and the way Crane seems determined to get him into bed. That’s definitely unusual.<br><br>
+        
+        Soon Stephen is falling hard for the worst possible man, at the worst possible time. But Crane’s dangerous appeal isn’t the only thing rendering Stephen powerless. Evil pervades the house, a web of plots is closing round Crane, and if Stephen can’t find a way through it—they’re both going to die.<br><br>
+        
+        This is a new edition of the book previously published by Samhain. The text has been lightly edited but there are no significant changes.",
+        :img_url => "../images/magpie-lord.jpg"
+    },
+    
+    "A Case Of Posession" => {
+        :author => "KJ Charles",
+        :summary => "<b>Magic in the blood. Danger in the streets.</b><br><br>
+
+        <i>A Charm of Magpies, Book 2</i><br><br>
+        
+        Lord Crane has never had a lover quite as elusive as Stephen Day. He knows Stephen’s job as justiciar requires secrecy, but the magician is doing his disappearing act more than seems reasonable—especially since Crane will soon return to his home in China. When a blackmailer threatens to expose their illicit relationship, there’s only one thing stopping Crane from leaving the country he loathes: Stephen.<br><br>
+        
+        Stephen has problems of his own. As he investigates a plague of giant rats sweeping London, his sudden increase in power, boosted by his blood-and-sex bond with Crane, is rousing suspicion that he’s turned warlock. With all eyes on him, the threat of exposure grows. Stephen could lose his friends, his job and his liberty over his relationship with Crane. He’s not sure if he can take that risk much longer. Crane isn’t sure if he can ask him to.<br><br>
+        
+        The rats are closing in, and something has to give…",
+        :img_url => "../images/case-of-posession.jpg"
+
     },
     "Flight of Magpies" => {
-        :author => "KJ Charles"
+        :author => "KJ Charles",
+        :summary => "<b>Danger in the air. Lovers on the brink.</b><br><br>
+        
+        <i>A Charm of Magpies, Book 1</i><br><br>
+
+        With the justiciary understaffed, a series of horrifying occult murders to be investigated, and a young student flying off the rails, magical law enforcer Stephen Day is under increasing stress. And the strain is starting to show in his relationship with his aristocratic lover, Lord Crane.<br><br>
+
+        Crane chafes at the restrictions of England’s laws, and there’s a worrying development in the blood-and-sex bond he shares with Stephen. A development that makes a sensible man question if they should be together at all.<br><br>
+        
+        Then a devastating loss brings the people he most loves into bitter conflict. Old enemies, new enemies, and unexpected enemies are painting Stephen and Crane into a corner, and the pressure threatens to tear them apart…",
+        :img_url => "../images/flight-of-magpies.jpg"
     },
+
     "Jackdaw" => {
-        :author => "KJ Charles"
+        :author => "KJ Charles",
+        :summary => "If you stop running, you fall.<br><br>
+
+        This story is linked to the Charm of Magpies series, coming after Flight of Magpies, but can be read as a standalone if you must.<br><br>
+        
+        Jonah Pastern is a magician, a liar, a windwalker, a professional thief…and for six months, he was the love of police constable Ben Spenser’s life. Until his betrayal left Ben jailed, ruined, alone, and looking for revenge.<br><br>
+        
+        Ben is determined to make Jonah pay. But he can’t seem to forget what they once shared, and Jonah refuses to let him. Soon Ben is entangled in Jonah’s chaotic existence all over again, and they’re running together—from the police, the justiciary, and some dangerous people with a lethal grudge against them.<br><br>
+        
+        Threatened on all sides by betrayals, secrets, and the laws of the land, can they find a way to live and love before the past catches up with them?",
+        :img_url => "../images/jackdaw.jpg"
     },
+
     "A Queer Trade" => {
-        :author => "KJ Charles"
+        :author => "KJ Charles",
+        :summary => "Apprentice magician Crispin Tredarloe returns to London to find his master dead, and his papers sold. Papers with secrets that could spell death. Crispin needs to get them back before anyone finds out what he’s been doing, or what his magic can do.<br><br>
+
+          Crispin tracks his quarry down to waste paper dealer Ned Hall. He needs help, and Ned can’t resist Crispin’s pleading—and appealing—looks. But can the waste-man and the magician prevent a disaster and save Crispin’s skin?<br><br>
+          
+          A 16,000 word story set in the Charm of Magpies world, and a prequel to the novel Rag and Bone (1 March 2016). This story was first published as part of the Charmed & Dangerous anthology.",
+        :img_url => "../images/queer-trade.jpg"
     },
+    
     "Rag and Bone" => {
-        :author => "KJ Charles"
+        :author => "KJ Charles",
+        :summary => "<b>It’s amazing what people throw away…</b><br><br>
+
+        Rag and Bone is a story set in the Charm of Magpies universe, featuring the characters who first appear in ‘A Queer Trade’.<br><br>
+        
+        Crispin Tredarloe never meant to become a warlock. Freed from his treacherous master, he’s learning how to use his magical powers the right way. But it’s brutally hard work. Not everyone believes he’s a reformed character, and the strain is putting unbearable pressure on his secret relationship with waste-man Ned Hall.<br><br>
+        
+        Ned’s sick of magic. Sick of the trouble it brings, sick of its dangerous grip on Crispin and the miserable look it puts in his eyes, and sick of being afraid that a gentleman magician won’t want a street paper-seller forever—or even for much longer.<br><br>
+        
+        But something is stirring among London’s forgotten discards. An ancient evil is waking up and seeking its freedom. And when wild magic hits the rag-and-bottle shop where Ned lives, a panicking Crispin falls back onto bad habits. The embattled lovers must find a way to work together—or London could go up in flames.",
+        :img_url => "../images/rag-and-bone.jpg"
     }
   }
 
