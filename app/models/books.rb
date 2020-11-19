a48596be930b871448f024af13c10755c63c24a6
ar00gula <arianna.mayer@gmail.com>
2020-11-19 14:07:03 -0500

    messing with the css - trying to remove any predone css that i don't need or understand

--- app/models/books.rb
+++ app/models/books.rb
@@ -2,4 +2,11 @@ class Book < ActiveRecord::Base
     has_many :book_tags
     has_many :tags, through: :book_tags
     belongs_to :user
+
+    # def sort_by_title(books)
+    #     books.sort do |book|
+    #         if self.title.split(" ")[0].downcase == "a" || self.title.split(" ")[0].downcase == "the"
+
+
+    #     end 
 end
